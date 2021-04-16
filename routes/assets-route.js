8363d5725199de2f0576592e51c56bf5ed4a89b6
Juan Delgado <roloenusa@msn.com>
2021-04-15 20:17:47 -0700

    Address comments

--- routes/assets-route.js
+++ routes/assets-route.js
@@ -7,10 +7,12 @@ const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
+const MAX_IMAGE_COUNT = 13;
+
 /**
  * Upload a number of images to the server
  */
-router.post('', upload.array('images', 13), async (req, res) => {
+router.post('', upload.array('images', MAX_IMAGE_COUNT), async (req, res) => {
   const { files } = req;
 
   const promises = await files.map(async (file) => {
