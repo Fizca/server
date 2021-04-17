3b625cdb373035b89e3aa5451eda1082514dc400
Juan Delgado <roloenusa@msn.com>
2021-04-16 21:45:53 -0700

    Add logout flow

--- routes/users-route.js
+++ routes/users-route.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const passport = require('../services/passport');
 
 const router = express.Router();
 
 /**
  * Retrieve own account
  */
-router.get('', async (req, res) => {
+router.get('/me', passport.isAuthenticated, async (req, res) => {
   const { user } = req;
   res.json(user);
 });
